ac48781cd6fa0aba73c1f5661ab66560de2782c8
cprpython25 <cpellico79@gmail.com>
2025-03-11 20:44:45 +0100

    martes11

--- proyectoweb/informacion/urls.py
+++ proyectoweb/informacion/urls.py
@@ -4,4 +4,5 @@ from informacion import views
 urlpatterns=[
     path('', views.index, name= 'index')
     path('pelis/, ')
+    path('', views.index, name=)
 ]
--- proyectoweb/informacion/views.py
+++ proyectoweb/informacion/views.py
@@ -10,3 +10,32 @@ def futbol(request):
         equipo": nombre
     }
     return render(request, 'informacion/futbol.html', data)
+
+def jugadores(request):
+    context = 
+
+def colores(request):
+    #RECUPERAMOS LA VARIABLE QUE NOS ESTAN ENVIANDO
+    #MEDIENATE GET (MICOLOR)
+    #DEBEMOS COMPROBAR QUE RECIBIMOS ALGO LLAMADO micolor
+    if ('micolor') in request.GET):
+    colorRecibido = request.GET['micolor']
+    #CON EL COLOR RECIBIDO SE LO DEVOLVEMOS AL DIBUJO
+    #PARA PINTARLO
+    context = {
+        "colordibujo": colorRecibido
+    }
+    return render(request, 'informacion/colores.html', context=)
+else:
+
+def saludo(request):
+    return render(request, 'informacion/saludo.html')
+    return render(request, 'informacion/colores.html')
+
+def saludo(request):
+    #PREGUNTAMOS DE FORMA OBLIGATORI SI HEMOS
+    #RECIBIDO DATOS DEL FORMULARIO
+    if ('cjasnombre' in request.POST):
+        nombreRecibido = request.POST['cajanombre']
+        context = {}
+
