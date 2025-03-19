ddca94fe756832e3a921bc86bb98dc17a952d17e
cprpython25 <cpellico79@gmail.com>
2025-03-19 19:52:44 +0100

    miercoles19bis

---
+++ proyectoapi/departamentos/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from departamentos.models import ServiceDepartamentos
+
+# Create your views here.
+def index(request):
+    servicio = ServiceDepartamentos()
+    departamentos = servicio.getDepartamentos()
+    context = {
+        "departamentos": departamentos
+    }
+    return render(request, 'pages/index.html', context)
+
+def insertarDepartamento(request):
+    if ('cajaid' in request.POST):
+        servicio = ServiceDepartamentos()
+        id = request.POST['cajaid']
+        nombre = request.POST['cajanombre']
+        localidad = request.POST['cajalocalidad']
+        servicio.insertarDepartamento(id, nombre, localidad)
+        departamentos = servicio.getDepartamentos()
+        context = {
+            "departamentos": departamentos
+        }
+        return render(request, 'pages/index.html', context)
+    else:
+        return render(request, 'pages/create.html')
+    
+def eliminar(request):
+    servicio = ServiceDepartamentos()
+    id = request.GET['id']
+    servicio.delete(id)
+    departamentos = servicio.getDepartamentos()
+    context = {
+        "departamentos": departamentos
+    }
+    return render(request, 'pages/index.html', context)
